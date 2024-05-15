f4867e20586b4fe9d0c0ef53319820704e2e8f21
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-05-15 13:48:53 -0300

    Introducing

---
+++ Data Types/Main.py
@@ -0,0 +1,20 @@
+# STRING
+name = str("Mr. Skywalker")
+name = "Mr. Skywalker"
+print(name)
+
+# INTEGER
+age = 29
+age = int(29)
+print(age)
+
+# FLOAT
+height = 1.70
+weight = float(80.2)
+print("Height: " + str(height) + ", Weight: " + str(weight))  # String manipulation
+
+
+
+# SHOWING THE DATA TYPE
+print(type(name))
+print(type(age))
