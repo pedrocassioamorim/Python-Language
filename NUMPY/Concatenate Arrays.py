dbf94e419adcf43ec657746518b2b7a2558306f3
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-06 22:15:39 -0300

    Arrays and concatenation, part 2

--- NUMPY/Concatenate Arrays.py
+++ NUMPY/Concatenate Arrays.py
@@ -11,12 +11,19 @@ d = np.concatenate((b, a))
 # EXEMPLE OF CONSULTING VALUES INSIDE ARRAYS
 e = np.arange(100).reshape(10, 10)
 
+# INSERINDO LÓGICA NA PROCURA DOS ELEMENTOS
+f = e[e > 45] # TODOS OS NUMEROS DE 'e' QUE SÃO MAIORES QUE 45
+g = f[f % 2 == 0] # TODOS OS NUMEROS DE 'f' QUE SÃO PARES
+
+
 # PRINTING
 print(a)
 print(b)
 print(c)
 print(d)
 print(e)
+print(f)
+print(g)
 
 #PRINTING
 print(e[4][5]) # consultando elemento da linha 4 x coluna 5
