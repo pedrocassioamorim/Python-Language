616addb5fa8c1dce6564312a633bed87f2e532a0
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-07 15:13:42 -0300

    Pandas Dataframes from different resources

--- PANDAS/Pandas Intro.py
+++ PANDAS/Pandas Intro.py
@@ -9,3 +9,11 @@ print(arquivo)
 outro = 'plants.ods'
 outro_arquivo = pd.read_excel(outro)
 print(outro_arquivo)
+
+# NOVOS EXEMPLOS DE DATAFRAMES
+brasil1 = pd.read_excel('selecao brasileira 2002 (excel).xlsx') # A PARTIR DE UMA PLANILHA DE EXCEL
+brasil2 = pd.read_csv('selecao brasileira 2002 (csv).csv') # A PARTIR DE UM ARQUIVO CSV
+
+print(brasil1.head()) # HEAD: imprime as primeias 5 linhas
+print(brasil2.head()) # HEAD: imprime as primeias 5 linhas
+print(brasil1.tail()) # TAIL: imprime as ultimas 5 linhas
