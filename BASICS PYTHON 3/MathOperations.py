c14dad222ee13a35da7943ec9631563c2fc83613
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-10 17:40:06 -0300

    Math operations

---
+++ BASICS PYTHON 3/MathOperations.py
@@ -0,0 +1,49 @@
+a = 10
+b = 20
+
+# SOMA
+soma = a + b
+print(type(soma))
+print(soma)
+
+# SUBTRAÇÃO
+sub = a - b
+print(type(sub))
+print(sub)
+
+# MULTIPLICAÇÃO
+mul = a * b
+print(type(mul))
+print(mul)
+
+# DIVISÃO
+div = a / b
+print(type(div))
+print(div)
+
+# DIVISÃO QUE GERA UM INTEIRO (TRUNCAMENTO)
+div2 = b // a
+print(type(div2))
+print(div2)
+
+# EXPONENCIAÇÃO
+c = 3
+exp = a ** c
+print(type(exp))
+print(exp)
+
+# MODULO (RESTO DA DIVISÃO)
+modulo = b % c
+print(type(modulo))
+print(modulo)
+
+# OPERAÇÕES MATEMÁTICAS:
+soma2 = a + b * c
+soma3 = (a + b) * c
+div2 = b // c
+print(type(soma2))
+print(soma2)
+print(type(soma3))
+print(soma3)
+print(type(div2))
+print(div2)
