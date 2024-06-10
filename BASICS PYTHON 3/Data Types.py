8123a997594a4a733799f9ece72a30abd7c5b0a9
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-10 17:29:14 -0300

    Basic Data Types

--- BASICS PYTHON 3/Data Types.py
+++ BASICS PYTHON 3/Data Types.py
@@ -13,8 +13,17 @@ height = 1.70
 weight = float(80.2)
 print("Height: " + str(height) + ", Weight: " + str(weight))  # String manipulation
 
-
+# BOOLEAN
+open = True
+close = False
+print(f" It's your name, {name}?")
+print(f" And is it true that you are {age} years old?")
+print(f" Yes, it's True, is the Party open?: {open}")
 
 # SHOWING THE DATA TYPE
 print(type(name))
 print(type(age))
+print(type(height))
+print(type(weight))
+print(type(open))
+print(type(close))
