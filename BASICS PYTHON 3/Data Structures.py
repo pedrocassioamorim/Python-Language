475c513f232673dc1696c900be9b163555533344
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-10 18:50:35 -0300

    Data Structures

---
+++ BASICS PYTHON 3/Data Structures.py
@@ -0,0 +1,38 @@
+# LISTAS - estrutura de dados ordenada
+notas = []
+notas = [9, 10, 8.5]
+
+print(notas[0])
+notas.append(40.6)
+print(notas)
+
+notas.sort() # ORDENAR CRESCENTEMENTE
+print(notas)
+
+notas.reverse() #REVERTENDO A ORDEM DA LISTA
+print(notas)
+
+ultimo = notas.pop() #REMOVENDO O ULTIMO ELETEMENTO
+print(notas)
+print(ultimo)
+
+# INSERINDO EM UMA POSIÇÃO ESPECIFICA
+notas.insert(2, 70) # Index 2 , objeto: 70
+print(notas)
+
+# REMOVENDO DE UMA POSIÇÃO ESPECIFICA
+notas.pop(0) # REMOVE ELEMENTO POSIÇÃO 0
+print(notas)
+
+# LISTA SIMPLES
+pessoa = ["Gabriel", 27, "(91) 93879-4434"]
+print(f'Nome: {pessoa[0]}')
+print(f'Idade: {pessoa[1]}')
+print(f'Telefone: {pessoa[2]}')
+
+# LISTA DE LISTAS
+pessoas = [
+    ["Gabriel", 27, "(91) 93879-4434"],
+    ["Bruno", 27, "(91) 93879-4434"],
+    ["Pedro", 28, "(91) 93879-4434"],
+]
